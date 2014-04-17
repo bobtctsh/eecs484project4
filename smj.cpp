2ac8dd0c15c1a48a88310466ea084ce34826412b
Bowen Ge <BowenGG@Bowens-MacBook-Pro.local>
2014-04-16 21:24:20 -0400

    smj

--- smj.cpp
+++ smj.cpp
@@ -2,6 +2,7 @@
 #include "query.h"
 #include "sort.h"
 #include "index.h"
+#include <cstring>
 
 /* Consider using Operators::matchRec() defined in join.cpp
  * to compare records when joining the relations */
@@ -19,13 +20,6 @@ Status Operators::SMJ(const string& result,           // Output relation name
   /* Your solution goes here */
 
     Status status;
-/*    HeapFileScan tmpfile1(attrDesc1.relName,status);
-    if(status != OK)
-        error.print(status);
-    HeapFileScan tmpfile2(attrDesc2.relName,status);
-    if(status != OK)
-        error.print(status);
-*/  HeapFile tmpResult(result,status);
     if(status != OK)
         error.print(status);
 
@@ -40,29 +34,21 @@ Status Operators::SMJ(const string& result,           // Output relation name
     max2 = tmpdesc[attrnum-1].attrOffset + tmpdesc[attrnum-1].attrLen;
     delete [] tmpdesc;
 
-    max1 = BufMgr::numUnpinnedPages*0.8*1024/max1;
-    max2 = BufMgr::numUnpinnedPages*0.8*1024/max2;
+    max1 = bufMgr->numUnpinnedPages()*0.8*1024/max1;
+    max2 = bufMgr->numUnpinnedPages()*0.8*1024/max2;
 
-    SortedFile tmpsort1(attrDesc1.relName,attrDesc1.attrOffset,attrDesc1.attrLen,attrDesc1.attrType,max1,status);
+    SortedFile tmpsort1(attrDesc1.relName,attrDesc1.attrOffset,attrDesc1.attrLen,static_cast<Datatype>(attrDesc1.attrType),max1,status);
     if(status != OK)
         error.print(status);
-    SortedFile tmpsort2(attrDesc2.relName,attrDesc2.attrOffset,attrDesc2.attrLen,attrDesc2.attrType,max2,status);
+    SortedFile tmpsort2(attrDesc2.relName,attrDesc2.attrOffset,attrDesc2.attrLen,static_cast<Datatype>(attrDesc2.attrType),max2,status);
     if(status != OK)
         error.print(status);
 
 
+    HeapFile tmpResult(result,status);
+    if(status != OK)
+        error.print(status);
 
- /*   if(attrDesc == NULL)
-    {
-        //get all the tuples;
-        tmpfile.startScan(0,1,DOUBLE,NULL,op);
-    }
-    else
-    {
-        //scan the heapfile to get qualify tuples
-        tmpfile.startScan(attrDesc->attrOffset,attrDesc->attrLen,static_cast<Datatype>(attrDesc->attrType),(char *)attrValue,op);
-    }
-*/
     Record rec1, rec2;
     status = tmpsort2.next(rec2);
     if(status != OK)
@@ -92,17 +78,15 @@ Status Operators::SMJ(const string& result,           // Output relation name
                 Record tmpRec;
                 tmpRec.data = new char[reclen];
                 tmpRec.length = reclen;
-                int offset = 0;
         
                 for(int i = 0; i < projCnt; i++)
                 {   
                     AttrDesc tmpDesc;
                     attrCat->getInfo(attrDescArray[i].relName,attrDescArray[i].attrName,tmpDesc);
-                    if (attrDescArray[i].relName == attrDesc1.relName) 
+                    if(memcmp(attrDescArray[i].relName,attrDesc1.relName,strlen(attrDesc1.relName))==0)
                         memcpy(tmpRec.data + attrDescArray[i].attrOffset, rec1.data + tmpDesc.attrOffset, attrDescArray[i].attrLen);
                     else
                         memcpy(tmpRec.data + attrDescArray[i].attrOffset, rec2.data + tmpDesc.attrOffset, attrDescArray[i].attrLen);
-                    offset += attrDescArray[i].attrLen;
                 }
                 RID tmpRid;
                 tmpResult.insertRecord(tmpRec,tmpRid);
