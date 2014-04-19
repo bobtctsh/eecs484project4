a24e174c74f1eff0490869dcde1ddb67c3ae0ecf
Bowen Ge <bobtctsh@umich.edu>
2014-04-18 21:38:50 -0400

    page added

--- page.cpp
+++ page.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string.h>
 #include "page.h"
-
+#include <cstring>
+#include "string.h"
 using namespace std;
 
 // page class constructor
@@ -11,6 +12,13 @@ using namespace std;
 void Page::init(int pageNo)
 {
     /* Solution Here */
+    curPage = pageNo; //initiate current page 
+    freeSpace = PAGESIZE-DPFIXED;// free space in bytes
+    freePtr = 0;
+    //init slot
+    slotCnt = -1;
+    slot[0].length=-1;
+    slot[0].offset=-1;
 }
 
 // dump page utlity
@@ -49,18 +57,60 @@ const int Page::getNextPage() const
 const short Page::getFreeSpace() const
 {
     /* Solution Here */
+    return freeSpace;
 }
     
 // Add a new record to the page. Returns OK if everything went OK
 // otherwise, returns NOSPACE if sufficient space does not exist.
 // RID of the new record is returned via rid parameter.
-// When picking a slot first check to see if any spots are avaialable 
+// When picking a slot first check tooffset see if any spots are avaialable 
 // in the middle of the slot array. Look from least negative to most 
 // negative.
 
 const Status Page::insertRecord(const Record & rec, RID& rid)
 {
     /* Solution Here */
+    //check different size
+    //wheter there is enough space for the new record
+    if(freeSpace <rec.length) 
+    {
+        return NOSPACE;
+    }
+    //slot[i] where i is negative to get the slot
+    for(int i = 0 ; i> slotCnt; i --)
+    {
+        if(slot[i].length==-1)
+        {
+            //find slot i which is unused
+            slot[i].offset=freePtr;
+            slot[i].length = rec.length;
+            rid.pageNo= curPage;
+            rid.slotNo=-i;
+            //insert the new record
+            memcpy(data+freePtr,(char*)rec.data,rec.length);
+    
+            freePtr +=rec.length;
+            freeSpace -= rec.length;
+
+            //evertthing done.
+            return OK;
+        }
+    }
+    // didnt find an avaliable slot, need to to assign a new one
+    // check to see if there is enough space for the record and the new slot
+    if(freeSpace <(short)rec.length + (short)sizeof(slot_t)) return NOSPACE;
+
+    //bookkeeping,new slot 'slotCnt' is used now
+    slot[slotCnt].offset = freePtr;
+    slot[slotCnt].length = rec.length;
+    rid.pageNo=curPage;
+    rid.slotNo = -slotCnt;
+    memcpy(data+freePtr,(char*)rec.data,rec.length);
+    freePtr += rec.length;
+    freeSpace -=rec.length + sizeof(slot_t);
+    slotCnt--;
+
+    return OK;
 }
 
 
@@ -72,6 +122,51 @@ const Status Page::insertRecord(const Record & rec, RID& rid)
 const Status Page::deleteRecord(const RID & rid)
 {
     /* Solution Here */
+    // is rid valid here
+    if(rid.pageNo != curPage || rid.slotNo>(- slotCnt) || rid.slotNo <0)
+    {
+        return INVALIDSLOTNO;
+    }
+
+    short tmpSlot = -rid.slotNo;
+    //empty the data;
+    memset(data+slot[tmpSlot].offset,0,slot[tmpSlot].length);
+    //need to compact after delete
+    short dest = slot[tmpSlot].offset;
+    short src = dest + slot[tmpSlot].length;
+    //
+    while(true)
+    {
+        if(src == freePtr)
+            break;
+        int k;
+        for(k = 0 ; k > slotCnt ; k--)
+        {
+            if(slot[k].offset == src)
+            {
+                slot[k].offset = dest;
+                break;
+            }
+        }
+        //move the record
+        bcopy(data+src,data+dest,slot[k].length);
+        dest += slot[k].length;
+        src += slot[k].length;
+    }
+    // bookkeeping 
+    freePtr -= slot[tmpSlot].length;
+    freeSpace += slot[tmpSlot].length;
+    slot[-rid.slotNo].length=-1;
+    slot[-rid.slotNo].offset =-1;
+    // are there any record in this page?
+    for(int i = 0 ; i > slotCnt ; i--)
+    {
+        if(slot[i].length !=-1)
+        {
+            return OK;
+        }
+    }
+    return NORECORDS; 
 }
 
 // returns RID of first record on page
@@ -79,6 +174,18 @@ const Status Page::deleteRecord(const RID & rid)
 const Status Page::firstRecord(RID& firstRid) const
 {
     /* Solution Here */
+    for(int i = 0 ; i > slotCnt ; i--)
+    {
+        if(slot[i].length !=-1)//start from slot[0] to slot[slotCnt]
+        {
+            firstRid.pageNo=curPage;
+            firstRid.slotNo=-i;
+            //find the first one
+            return OK;
+        }
+    }
+    return NORECORDS; 
+
 }
 
 // returns RID of next record on the page
@@ -86,11 +193,45 @@ const Status Page::firstRecord(RID& firstRid) const
 const Status Page::nextRecord (const RID &curRid, RID& nextRid) const
 {
     /* Solution Here */
+    //check Rid
+    //no more records exist on the page
+    if(curRid.pageNo == -1)
+    {
+        return ENDOFPAGE;
+    }
+    //whethre curRid is invalid here
+    if(curRid.pageNo != curPage || curRid.slotNo>(- slotCnt) || curRid.slotNo <0)
+    {
+        return INVALIDSLOTNO;
+    }
+
+    for(int i = -curRid.slotNo-1; i> slotCnt ; i--)
+    {
+        if(slot[i].length != -1)
+        {
+            nextRid.pageNo= curPage;
+            nextRid.slotNo= -i;
+            return OK;
+        }
+    }
+    return ENDOFPAGE;
 }
 
 // returns length and pointer to record with RID rid
 // returns OK on success and INVALIDSLOTNO if invalid rid 
 const Status Page::getRecord(const RID & rid, Record & rec)
 {
-    /* Solution Here */
+    /* Solution Here */ 
+    if(rid.pageNo != curPage || rid.slotNo>(- slotCnt) || rid.slotNo <0)
+    {
+        return INVALIDSLOTNO;
+    }
+    short offset = slot[-rid.slotNo].offset;
+    short length = slot[-rid.slotNo].length;
+    char *dataRec = new char [length];
+    //
+    memcpy(dataRec,data+offset,length);
+    rec.length = length;
+    rec.data = (void*) dataRec;
+    return OK;
 }
